e17b23fdf0bad58510914c6b00b6cc457f3ed36c
mr.Blue <yashinsergey125@gmail.com>
2020-01-21 14:34:39 +0300

    add pattern "command"

---
+++ src/behavioral/command/Database.java
@@ -0,0 +1,17 @@
+package behavioral.command;
+
+public class Database {
+
+    public void insert() {
+        System.out.println("Inserting record...");
+    }
+    public void select() {
+        System.out.println("Reading record...");
+    }
+    public void update() {
+        System.out.println("Updating record...");
+    }
+    public void delete() {
+        System.out.println("Deleting record...");
+    }
+}
